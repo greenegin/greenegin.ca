92dfe686c58ad516fd75b087a37ba9c41994dfa6
Negin <negin@greenegin.ca>
2025-01-21 17:17:15 -0800

    Update 4
    Second title removed

--- components/landing_page.tsx
+++ components/landing_page.tsx
@@ -36,9 +36,7 @@ export function Landing_page() {
           </div>
 
           <h1 className="relative z-10 text-5xl md:text-7xl font-bold text-amber-300 text-center px-4 pt-12 md:pt-16 tracking-wide">
-              DISCOVER
-              THE
-              ARTS OF JAPAN
+              DISCOVER THE ARTS OF JAPAN
           </h1>
 
           <div className="flex-grow relative z-10 w-full">
@@ -101,11 +99,6 @@ export function Landing_page() {
                   </div>
               </Link>
           </div>
-        {/*
-          <h2 className="relative z-10 text-5xl md:text-7xl font-bold text-amber-300 text-center px-4 pb-12 md:pb-16 tracking-wide">
-              Arts of Japan
-          </h2>
-          */}
       </div>
   )
 }
