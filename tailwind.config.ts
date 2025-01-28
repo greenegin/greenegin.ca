d4e0e23d0b074776b34b0f55738a398abcdfad3d
Negin <negin@greenegin.ca>
2025-01-28 01:19:13 -0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -85,7 +85,7 @@ const config: Config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        'orbit-1': 'orbit-1 45s linear infinite',
+        'orbit-1': 'orbit-1 30s linear infinite',
         'orbit-2': 'orbit-2 45s linear infinite',
       },
     },
