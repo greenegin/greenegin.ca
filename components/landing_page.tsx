b230a4a75579849fdd633a49c49ae239f6088c8e
Negin <negin@greenegin.ca>
2025-04-14 21:53:17 -0700

    Update landing_page.tsx

--- components/landing_page.tsx
+++ components/landing_page.tsx
@@ -72,13 +72,13 @@ export function Landing_page() {
               </Link>
           </div>
 
-        {/* Comment */}
+        {/* Comment 
          <div className="absolute bottom-0 w-full z-10 text-center px-4 pb-4 md:pb-8">
               <p className="text-lg md:text-l">
                   We are yet to establish our perfect web presence, meanwhile enjoy our instagram profiles.
               </p>
           </div>
-    
+        */}
       </div>
   )
 }
